15879763a6fb09655fb09f1f6d8b168cdbcfd4c1
carry156176 <carry156176@163.com>
2020-06-11 10:38:14 +0800

    查看轮询构造是否成功

--- run_suite.py
+++ run_suite.py
@@ -8,6 +8,8 @@ suit = unittest.TestLoader().discover(app.BASE_DIR + "/script", pattern="test*.p
 # 定义存放测试报告的文件名
 report_path = app.BASE_DIR + "/report/time{}.html".format(time.strftime("%Y%m%d%H%M%S"))
 
+print("查看轮询构造是否成功")
+
 # 2、使用HTMLTestRunner生成测试报告
 with open(report_path, "wb") as f:
     runner = HTMLTestRunner_PY3.HTMLTestRunner(f, verbosity=1, title="登录接口测试", description="登录接口测试报告")
